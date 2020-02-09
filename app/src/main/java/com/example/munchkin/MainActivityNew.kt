ca010db94664ef50b6bdfc36a281638b8e651d54
Brankale <45881310+Brankale@users.noreply.github.com>
2020-02-09 20:57:31 +0100

    improved GUI

--- app/src/main/java/com/example/munchkin/MainActivityNew.kt
+++ app/src/main/java/com/example/munchkin/MainActivityNew.kt
@@ -3,18 +3,30 @@ package com.example.munchkin
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.munchkin.custom_views.PlayerView
 
 
 class MainActivityNew : AppCompatActivity() {
 
     private val TAG: String = MainActivityNew::class.java.simpleName
 
+    private lateinit var playersView: ListView
+    private lateinit var adapter: ArrayAdapter<PlayerView>
+    private lateinit var players: ArrayList<PlayerView>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_new)
-
         setSupportActionBar(findViewById(R.id.my_toolbar))
+
+        playersView = findViewById(R.id.player_list)
+        players = ArrayList()
+        adapter = ArrayAdapter(this, R.layout.player_view, players)
+        playersView.adapter = adapter
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -22,12 +34,26 @@ class MainActivityNew : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.add_player -> TODO("implement add player")
-            R.id.remove_player -> TODO("implement remove player")
-            else -> return super.onOptionsItemSelected(item)
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.add_player -> {
+            addPlayer()
+            true
         }
+        R.id.remove_player -> {
+            removePlayer()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun addPlayer() {
+//        players.add(PlayerView(this, null))
+//        adapter.notifyDataSetChanged()
+    }
+
+    private fun removePlayer() {
+//        players.removeAt(players.size - 1)
+//        adapter.notifyDataSetChanged()
     }
 
 }
